f75216cde097ff80fbe2725176990e87f15c24af
miccar <michele.carillo@gmail.com>
2015-02-23 15:56:28 +0100

    up

--- src/main/java/it/isislab/sof/client/application/SOFClientUI.java
+++ src/main/java/it/isislab/sof/client/application/SOFClientUI.java
@@ -23,7 +23,7 @@ public class SOFClientUI {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		SOFClientUI ui = new SOFClientUI("scud-resources/SCUD.jar","scud-resources/SCUD-RUNNER.jar");
+		SOFClientUI ui = new SOFClientUI("sof-resources/SCUD.jar","sof-resources/SCUD-RUNNER.jar");
 
 	}
 }
